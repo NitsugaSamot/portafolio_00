18783b959f38c5c621441677ebd638c8f48ebf95
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-10-06 13:42:51 -0300

    agregando experiencia de usuario mas fluida

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
-        document.addEventListener('DOMContentLoaded', function(){
+// Obtenemos una referencia al botón y al componente 'div'
+
+const toggleButton = document.getElementById('toggle-about');
+const aboutMeDiv = document.getElementById('about-me');
+const projectsContainer = document.getElementById('projects-container') 
+const toggleButtonProjects = document.getElementById('toggle-about-projects')
+
+// Agregamos un controlador de evento al botón para mostrar/ocultar el componente 'div'
+
+toggleButton.addEventListener('click', function() {
+    if (aboutMeDiv.style.display === 'none' || aboutMeDiv.style.display === '') {
+        aboutMeDiv.style.display = 'block'; // Mostrar 'aboutMeDiv'
+        projectsContainer.style.display = 'none'; // Ocultar 'projectsContainer'
+    } 
+});
+
+toggleButtonProjects.addEventListener('click', function() {
+    if (projectsContainer.style.display === 'none' || projectsContainer.style.display === '') {
+        projectsContainer.style.display = 'flex'; // Mostrar 'projectsContainer'
+        aboutMeDiv.style.display = 'none'; // Ocultar 'aboutMeDiv'
+    }
+});
+
+// toggleButton.addEventListener('click', function() {
+//     if (aboutMeDiv.style.display === 'none' || aboutMeDiv.style.display === '') {
+//         aboutMeDiv.style.display = 'block'; // Mostrar el componente 'div'
+//     } else {
+//         aboutMeDiv.style.display = 'none'; // Ocultar el componente 'div'
+//     }
+// });
+
+
+// toggleButtonProjects.addEventListener('click', function() {
+//     if (projectsContainer.style.display === 'none' || projectsContainer.style.display === '') {
+//         projectsContainer.style.display = 'flex'; // Mostrar el componente 'div'
+//     } else {
+//         projectsContainer.style.display = 'none'; // Ocultar el componente 'div'
+//     }
+// });
+
+
+
+      
+      document.addEventListener('DOMContentLoaded', function(){
         verModalAPV()
         clickCerrarModal()
     })
